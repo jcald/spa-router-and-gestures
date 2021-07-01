77eb6aae332e62f31108844d9bc4d072c5f3ed87
José Calderón Celis <josecalderoncelis@gmail.com>
2021-07-01 12:09:01 -0500

    Inicio Tailwind

--- src/Routes/Home.svelte
+++ src/Routes/Home.svelte
@@ -7,17 +7,22 @@
 </script>
 
 <h1>Home</h1>
-<div id="nav">
-  <a href="/about" class="href" use:link> To About Page</a>
-  <a href="/items/{contador}" class="href" use:link> To Items Page</a>
-  <a href="/login/{name}/{pass}" class="href" use:link> To login Page</a>
-  <a href="/Doubletap" class="href" use:link>Doubletap Page</a>
-  <a href="/Error" class="href" use:link>Error Page</a>
-  <a href="/Pan" class="href" use:link>Pan Page</a>
-  <a href="/Pinch" class="href" use:link>Pinch Page</a>
-  <a href="/Rotation" class="href" use:link>Rotation Page</a>
-  <a href="/Swipe" class="href" use:link>Swipe Page</a>
-  <a href="/Tap" class="href" use:link>Tap Page</a>
+<div id="nav" class="bg-blue-900 shadow-lg">
+  <a href="/about" class="p-3 hover:text-red-900" use:link> To About Page</a>
+  <a href="/items/{contador}" class="p-3 hover:text-red-900" use:link>
+    To Items Page</a
+  >
+  <a href="/login/{name}/{pass}" class="p-3 hover:text-red-900" use:link>
+    To login Page</a
+  >
+  <a href="/Doubletap" class="p-3 hover:text-red-900" use:link>Doubletap Page</a
+  >
+  <a href="/Error" class="p-3 hover:text-red-900" use:link>Error Page</a>
+  <a href="/Pan" class="p-3 hover:text-red-900" use:link>Pan Page</a>
+  <a href="/Pinch" class="p-3 hover:text-red-900" use:link>Pinch Page</a>
+  <a href="/Rotation" class="p-3 hover:text-red-900" use:link>Rotation Page</a>
+  <a href="/Swipe" class="p-3 hover:text-red-900" use:link>Swipe Page</a>
+  <a href="/Tap" class="p-3 hover:text-red-900" use:link>Tap Page</a>
 </div>
 <button class="btn btn-danger" on:click={() => (contador += 1)}>
   Contador ++
@@ -27,4 +32,8 @@
   #nav {
     flex-flow: row;
   }
+
+  /* #nav {
+    @aply bg-blue-900 shadow-lg;
+  } */
 </style>
